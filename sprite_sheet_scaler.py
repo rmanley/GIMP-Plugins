f9ee503747c6a58318c20794ac981799f314eab0
rmanley <canaanm1114@gmail.com>
2020-01-19 21:25:07 -0600

    Added documentation.

--- sprite_sheet_scaler.py
+++ sprite_sheet_scaler.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+# sprite_sheet_scaler.py: GIMP plug-in for scaling sprite sheets without blurring.
+
+# Copyright (C) 2020 by Canaan Manley
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from gimpfu import *
 
 def scale_sprite_sheet(image, layer, oldSpriteWidth, oldSpriteHeight, newSpriteWidth, newSpriteHeight):
